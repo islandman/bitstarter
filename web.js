cb47010d842f26fa804e5a55c9588cbede8d7f53
Ubuntu <ubuntu@ip-172-31-38-62.us-west-2.compute.internal>
2013-07-13 17:53:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.configure(function(){
-    app.use('/', express.static('index.html'));
-    app.use(express.static('index.html'));
+    app.use('/', express.static(__dirname));
+    app.use(express.static(__dirname));
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
