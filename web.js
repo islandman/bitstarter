25dbec46f0db6285f3d361498ff18ae0d72bfced
Ubuntu <ubuntu@ip-172-31-38-62.us-west-2.compute.internal>
2013-07-13 17:33:36 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 
+ 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.configure(function(){
+    app.use('/', express.static('index.html'));
+    app.use(express.static('index.html'));
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
